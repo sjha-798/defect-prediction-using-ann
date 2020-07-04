998e3841c2eb37d8a2a61556c33cf039e8739a12
sjha-798 <57917768+sjha-798@users.noreply.github.com>
2020-07-04 17:54:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ df
 
 dataset = df.values
 dataset
-
+#First 8 columns of the datset are the independent variables and the last column is dependent.
 X = dataset[:,0:9]
 Y = dataset[:,9]
 
@@ -19,16 +19,15 @@ X_scale = min_max_scaler.fit_transform(X)
 X_scale
 
 from sklearn.model_selection import train_test_split
-
+#splitting dataset into test and train
 X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X_scale, Y, test_size=0.2)
-
 X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
 
 print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
 
 from keras.models import Sequential
 from keras.layers import Dense
-
+#Making a model with one hidden layer and 32 neurons in each layer
 model = Sequential([
     Dense(32, activation='relu', input_shape=(9,)),
     Dense(32, activation='relu'),
@@ -38,7 +37,7 @@ model = Sequential([
 model.compile(optimizer='sgd',
               loss='binary_crossentropy',
               metrics=['accuracy'])
-
+#Training the model
 hist = model.fit(X_train, Y_train,
           batch_size=32, epochs=100,
           validation_data=(X_val, Y_val))
@@ -46,7 +45,7 @@ hist = model.fit(X_train, Y_train,
 model.evaluate(X_test, Y_test)[1]
 
 import matplotlib.pyplot as plt
-
+#To visualize loss
 plt.plot(hist.history['loss'])
 plt.plot(hist.history['val_loss'])
 plt.title('Model loss')
@@ -54,7 +53,7 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Val'], loc='upper right')
 plt.show()
-
+#To visualize accuracy
 plt.plot(hist.history['accuracy'])
 plt.plot(hist.history['val_accuracy'])
 plt.title('Model accuracy')
@@ -64,7 +63,7 @@ plt.legend(['Train', 'Val'], loc='lower right')
 plt.show()
 
 predictions = model.predict_classes(X)
-
+#check predicted values
 for i in range(10):
 	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], Y[i]))
 
